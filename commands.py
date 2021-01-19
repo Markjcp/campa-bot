7952fffe3707ceed5db526cee90ddd08d04583b1
Marcos Forlenza <mforlenza@mulesoft.com>
2021-01-19 00:43:47 -0300

    Feedback command added

--- commands.py
+++ commands.py
@@ -4,7 +4,7 @@ import json
 import api_ids as ids
 
 START_MSG = 'Hi. I am CampaBot(BETA Version). I will keep you updated about Facundo Campazzo NBA games. Please go to /help to find what can I do for you.'
-HELP_MSG = 'Commands available: \n/today - checks if today Denver is playing \n/stats - gives the stats for Campazzo in a given day\n/season - all Facu games in the season\n/about - about this bot\nfeedback - tell us what you think'
+HELP_MSG = 'Commands available: \n/today - checks if today Denver is playing \n/stats - gives the stats for Campazzo in a given day\n/season - all Facu games in the season\n/about - about this bot\n/feedback - tell us what you think'
 ABOUT_MSG = 'This bot is in BETA Version. No optimizations are being made. All the data is from https://rapidapi.com/theapiguy/api/free-nba (unofficial).'
 NO_GAME_MSG = 'No luck, Culeao!!'
 FEEDBACK_MSG = 'Give us feedback in our public channel: https://t.me/CampaBotFeedback'
